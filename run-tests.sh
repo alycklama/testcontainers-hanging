eff4a27251a7ec67a35984000bc392df7f5bb31f
alycklama <>
2024-11-22 14:24:29 +0100

    Updates `run-tests.sh` to route stdout to log file via `tee`

--- run-tests.sh
+++ run-tests.sh
@@ -9,7 +9,7 @@ rm -rf logs
 mkdir logs
 for ((i=1; i<=n; i++))
 do
-  npx vitest run src/__test__/testExample.test.ts | tee -a "logs/vitest-$test_id-$i.log"
+  npx vitest run src/__test__/testExample.test.ts 2>&1 | tee "logs/vitest-$test_id-$i.log"
 done
 
 ls logs/vitest-* | wc -l
